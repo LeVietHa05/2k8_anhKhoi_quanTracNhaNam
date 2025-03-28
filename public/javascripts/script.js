35601c5b8ee5ea0223a1e8ec8a6d4594fffbb6d8
LeVietHa05 <ha.leviet.0504@gmail.com>
2025-03-28 17:47:15 +0700

    refactor chart initialization and update logic; streamline data fetching and rendering process

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,54 +1,27 @@
+let ctxs_temp = document.querySelectorAll("div.charts canvas");
+// console.log(ctxs_temp);
+let ctxs = [];
+for (let ctx of ctxs_temp) {
+    ctxs.push(ctx.getContext("2d"));
+}
+// console.log(ctxs);
+// const ctx1 = document.getElementById("chart1").getContext("2d");
+// const ctx2 = document.getElementById("chart2").getContext("2d");
+// const ctx3 = document.getElementById("chart3").getContext("2d");
+// const ctx4 = document.getElementById("chart4").getContext("2d");
+
+let labels = ["00:00", "01:00", "02:00", "03:00", "04:00"];
+let datas = [
+    [[15, 20, 15, 30, 25], [10, 25, 5, 38, 75]],
+    [[15, 20, 15, 30, 25], [10, 25, 5, 38, 75]],
+    [[15, 20, 15, 30, 25], [10, 25, 5, 38, 75]],
+    [[10, 20, 15, 30, 25]]
+];
+const colors = [["red", "green"], ["blue", "orange"], ["green", "black"], ["purple"]];
+const chartLabels = [["Nhiệt độ", "Độ ẩm"], ["Nhiệt độ đất", "Độ ẩm đất"], ["PM10", "PM2.5"], ["Chất lượng không khí"]];
+const charts = [];
 document.addEventListener("DOMContentLoaded", async function () {
-    let ctxs_temp = document.querySelectorAll("div.charts canvas");
-    // console.log(ctxs_temp);
-    let ctxs = [];
-    for (let ctx of ctxs_temp) {
-        ctxs.push(ctx.getContext("2d"));
-    }
-    // console.log(ctxs);
-    // const ctx1 = document.getElementById("chart1").getContext("2d");
-    // const ctx2 = document.getElementById("chart2").getContext("2d");
-    // const ctx3 = document.getElementById("chart3").getContext("2d");
-    // const ctx4 = document.getElementById("chart4").getContext("2d");
 
-    let labels = ["00:00", "01:00", "02:00", "03:00", "04:00"];
-    let datas = [
-        [[15, 20, 15, 30, 25], [10, 25, 5, 38, 75]],
-        [[15, 20, 15, 30, 25], [10, 25, 5, 38, 75]],
-        [[15, 20, 15, 30, 25], [10, 25, 5, 38, 75]],
-        [[10, 20, 15, 30, 25]]
-    ];
-    const colors = [["red", "green"], ["blue", "orange"], ["green", "black"], ["purple"]];
-    const chartLabels = [["Nhiệt độ", "Độ ẩm"], ["Nhiệt độ đất", "Độ ẩm đất"], ["PM10", "PM2.5"], ["Chất lượng không khí"]];
-    const charts = [];
-    setInterval(async () => {
-        await fetch("/readings?limit=10")
-            .then(response => response.json())
-            .then(data => {
-                labels = data.labels;
-                datas = data.datas;
-            });
-
-        for (let i = 0; i < ctxs.length; i++) {
-            const ctx = ctxs[i];
-            const label = chartLabels[i];
-            const color = colors[i];
-            const data = datas[i];
-
-            charts[i].data.labels = labels;
-            charts[i].data.datasets = data.map((d, i) => ({
-                label: label[i],
-                data: d,
-                borderColor: color[i],
-                borderWidth: 1,
-                fill: false
-            }));
-            
-
-            charts[i].update();
-        }
-
-    }, 10000);
 
     await fetch("/readings?limit=10")
         .then(response => response.json())
@@ -147,3 +120,32 @@ function removeData(chart) {
     });
     chart.update();
 }
+
+setInterval(async () => {
+    await fetch("/readings?limit=10")
+        .then(response => response.json())
+        .then(data => {
+            labels = data.labels;
+            datas = data.datas;
+        });
+
+    for (let i = 0; i < ctxs.length; i++) {
+        const ctx = ctxs[i];
+        const label = chartLabels[i];
+        const color = colors[i];
+        const data = datas[i];
+
+        charts[i].data.labels = labels;
+        charts[i].data.datasets = data.map((d, i) => ({
+            label: label[i],
+            data: d,
+            borderColor: color[i],
+            borderWidth: 1,
+            fill: false
+        }));
+
+
+        charts[i].update();
+    }
+
+}, 10000);
